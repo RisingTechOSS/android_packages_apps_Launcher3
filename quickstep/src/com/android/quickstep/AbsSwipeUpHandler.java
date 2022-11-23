29a586ae32a2db92483cec356ab06bb978ad8da8
hydrangea07 <104114625+hydrangea07@users.noreply.github.com>
2022-11-23 23:03:33 +0800

    Launcher3: Clear task snapshots remnants on 'onRecentsAnimationComplete'
    - to avoid remnants on gesture start/swipe up
    
    Change-Id: Ife5f05263e07afec2456c535734f6c12e7080c9c
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- quickstep/src/com/android/quickstep/AbsSwipeUpHandler.java
+++ quickstep/src/com/android/quickstep/AbsSwipeUpHandler.java
@@ -526,7 +526,9 @@ public abstract class AbsSwipeUpHandler<T extends StatefulActivity<S>,
                 mRecentsView.switchToScreenshot(snapshots, () -> {
                     if (mRecentsAnimationController != null) {
                         mRecentsAnimationController.cleanupScreenshot();
-                    } else if (mDeferredCleanupRecentsAnimationController != null) {
+                        mRecentsAnimationController = null;
+                    }
+                    if (mDeferredCleanupRecentsAnimationController != null) {
                         mDeferredCleanupRecentsAnimationController.cleanupScreenshot();
                         mDeferredCleanupRecentsAnimationController = null;
                     }
@@ -2296,7 +2298,6 @@ public abstract class AbsSwipeUpHandler<T extends StatefulActivity<S>,
 
     @Override
     public void onRecentsAnimationFinished(RecentsAnimationController controller) {
-        mRecentsAnimationController = null;
         mRecentsAnimationTargets = null;
         if (mRecentsView != null) {
             mRecentsView.setRecentsAnimationTargets(null, null);
