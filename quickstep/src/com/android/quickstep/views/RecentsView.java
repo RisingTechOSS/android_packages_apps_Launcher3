84744f4ac1b6fc27386c8424e3d071c8f3578673
minaripenguin <minaripenguin@users.noreply.github.com>
2023-03-03 21:51:05 +0800

    Launcher3: Improve lock recent tasks feature
    * dynamically render lock and unlock icon
    * update lock-unlock icons with a more noticeable transition
    
    Co-authored-by: Anas Karbila <anaskarbila@gmail.com>
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- quickstep/src/com/android/quickstep/views/RecentsView.java
+++ quickstep/src/com/android/quickstep/views/RecentsView.java
@@ -122,6 +122,7 @@ import android.view.accessibility.AccessibilityEvent;
 import android.view.accessibility.AccessibilityNodeInfo;
 import android.view.animation.Interpolator;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.OverScroller;
 import android.widget.Toast;
@@ -721,12 +722,9 @@ public abstract class RecentsView<ACTIVITY_TYPE extends StatefulActivity<STATE_T
     private float mActionsViewAlphaAnimatorFinalValue;
     private MemInfoView mMemInfoView;
 
-    Drawable mLockedDrawable;
-    Drawable mUnlockedDrawable;
-
     List<String> mLockedTasks = new ArrayList<>();
 
-    private Button mLockButtonView;
+    private ImageButton mLockButtonView;
 
     private String mStartPkg, mEndPkg;
 
@@ -829,8 +827,6 @@ public abstract class RecentsView<ACTIVITY_TYPE extends StatefulActivity<STATE_T
         if (mLockedTasks.size() == 0 && lockedTasks != null && !lockedTasks.isEmpty()) {
             mLockedTasks = new ArrayList<String>(Arrays.asList(lockedTasks.split(",")));
         }
-        mLockedDrawable = context.getDrawable(R.drawable.recents_locked);
-        mUnlockedDrawable = context.getDrawable(R.drawable.recents_unlocked);
 
         mTintingColor = getForegroundScrimDimColor(context);
 
@@ -1032,8 +1028,9 @@ public abstract class RecentsView<ACTIVITY_TYPE extends StatefulActivity<STATE_T
         mActionsView.updateHiddenFlags(HIDDEN_NO_TASKS, getTaskViewCount() == 0);
         mSplitSelectStateController = splitController;
         mMemInfoView = memInfoView;
-        mLockButtonView = (Button) mActionsView.findViewById(R.id.action_lock);
+        mLockButtonView = (ImageButton) mActionsView.findViewById(R.id.action_lock);
         mLockButtonView.setOnClickListener(this::lockCurrentTask);
+        mLockButtonView.setImageResource(R.drawable.recents_unlocked);
     }
 
     public SplitSelectStateController getSplitSelectController() {
@@ -1371,6 +1368,9 @@ public abstract class RecentsView<ACTIVITY_TYPE extends StatefulActivity<STATE_T
         super.onPageEndTransition();
         if (getCurrentPageTaskView() != null) {
             mEndPkg = getCurrentPageTaskView().getTask().key.getPackageName();
+        }
+        if (mLockedTasks.contains(mStartPkg) != mLockedTasks.contains(mEndPkg)) {
+            updateLockIcon();
         }
         if (getNextPage() > 0) {
             setSwipeDownShouldLaunchApp(true);
@@ -4043,7 +4043,7 @@ public abstract class RecentsView<ACTIVITY_TYPE extends StatefulActivity<STATE_T
         }
     }
 
-    private void lockCurrentTask(View view) {
+    public void lockCurrentTask(View view) {
         TaskView taskView = getCurrentPageTaskView();
         if (taskView != null) {
             Task t = taskView.getTask();
@@ -4053,10 +4053,21 @@ public abstract class RecentsView<ACTIVITY_TYPE extends StatefulActivity<STATE_T
             } else {
                 mLockedTasks.add(pkg);
             }
+            updateLockIcon(pkg);
         }
         Settings.System.putStringForUser(getContext().getContentResolver(),
         Settings.System.RECENTS_LOCKED_TASKS, String.join(",", mLockedTasks),
                 UserHandle.USER_CURRENT);
+    }
+
+    private void updateLockIcon() {
+        if (getNextPageTaskView() != null)
+            updateLockIcon(getNextPageTaskView().getTask().key.getPackageName());
+    }
+
+    private void updateLockIcon(String pkg) {
+        boolean isLocked = mLockedTasks.contains(pkg);
+        mLockButtonView.setImageResource(isLocked ? R.drawable.recents_locked : R.drawable.recents_unlocked);
     }
 
     @Override
@@ -4273,6 +4284,7 @@ public abstract class RecentsView<ACTIVITY_TYPE extends StatefulActivity<STATE_T
         setImportantForAccessibility(isModal() ? IMPORTANT_FOR_ACCESSIBILITY_NO
                 : IMPORTANT_FOR_ACCESSIBILITY_AUTO);
         doScrollScale();
+        updateLockIcon();
     }
 
     private void updatePivots() {
@@ -5067,6 +5079,7 @@ public abstract class RecentsView<ACTIVITY_TYPE extends StatefulActivity<STATE_T
         updateCurrentTaskActionsVisibility();
         loadVisibleTaskData(TaskView.FLAG_UPDATE_ALL);
         updateEnabledOverlays();
+        updateLockIcon();
     }
 
     @Override
