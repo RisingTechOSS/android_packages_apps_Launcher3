cb6682f2360ffcbf2b3dde7f302bcda962e286a2
minaripenguin <minaripenguin@users.noreply.github.com>
2023-04-13 19:05:36 +0800

    OrtusLauncher: Introduce Shake Gestures
    * inspired from motorola chop gesture
    
    Change-Id: I1ebd7085c4f5234c61a951df5b372e746a0fe62d
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- src/com/android/launcher3/settings/SettingsHomescreen.java
+++ src/com/android/launcher3/settings/SettingsHomescreen.java
@@ -110,6 +110,7 @@ public class SettingsHomescreen extends CollapsingToolbarBaseActivity
             case Utilities.KEY_SINGLE_PAGE_CENTER:
             case Utilities.KEY_VIBRATION_TOGGLE:
             case Utilities.KEY_FORCE_MONOCHROME_ICONS:
+            case Utilities.KEY_SHAKE_GESTURES_INTENSITY:
                 LauncherAppState.getInstanceNoCreate().setNeedsRestart();
                 break;
             default:
